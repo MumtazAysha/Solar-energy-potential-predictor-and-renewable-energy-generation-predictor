63b61f490633e6309e845d8d2d795625950becc4
MumtazAysha <mumtazaysha9@gmail.com>
2025-12-24 11:56:56 +0530

    Created an init method to stre the inital values of longitueds and latitudes of Sri Lanka

--- scripts/grid_generator.py
+++ scripts/grid_generator.py
@@ -12,5 +12,13 @@ import math
 class SriLankaGrid:
     """creates 10km x 10km grid cells for Sri Lanka"""
 
-    
+    def __init__(self):
+        #Sri Lanka bounds: 5°55′–9°51′N, 79°41′–81°53′E
+        self.minlat = 5.917 # 5°55'N
+        self.maxlat = 9.850 # 9°51'N
+        self.minlon = 79.683 # 79°41'E
+        self.maxlon = 81.883 # 81°53'E
+        
+
+
 
