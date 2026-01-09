2d738378d2220c69afc26cb64361b1e5ac0b7597
MumtazAysha <mumtazaysha9@gmail.com>
2026-01-09 16:25:58 +0530

    created reverse geocoding

--- scripts/get_location_names.py
+++ scripts/get_location_names.py
@@ -8,4 +8,23 @@ GOOGLE_API_KEY = "AIzaSyD07sbzMiYSXoTsByLl4M8Z-fqovp_mrlE"  # Get from: https://
 def get_exxcaact_location_google(lat, loon, gmaps_client):
     """Get detailed locatin using Google MAps"""
 
+    try:
+         #Reverse Geocode
+         results = gmaps_client.reverse_geocode((lat,loon), language='en')
+
+         if not results:
+             return None
+         
+         
     
+
+
+
+
+
+
+
+
+
+    except:
+        return None
